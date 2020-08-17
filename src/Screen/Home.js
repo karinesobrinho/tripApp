71b2aaf3a64c54bcca8e1045968e3d6044604837
Karine Sobrinho <karine.silva.sobrinho@gmail.com>
2020-08-17 18:51:31 -0300

    botoes home

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -2,11 +2,26 @@ import React from 'react'
 import {View, StyleSheet, Text, Image, ScrollView, SafeAreaView, Button} from 'react-native'
 import styles from '../Components/style'
 
-const Home = ()=>{
+const Home = ({navigation})=>{
     return(
         <View>
             <Text>Home</Text>
-           
+            <Button
+                title ='Voos'
+                onPress = {()=> navigation.navigate('Voos')}
+            />
+             <Button
+                title ='Hospedagens'
+                onPress = {()=> navigation.navigate('Hospedagens')}
+            />
+             <Button
+                title ='Pacotes'
+                onPress = {()=> navigation.navigate('Pacotes')}
+            />
+             <Button
+                title ='Atividades'
+                onPress = {()=> navigation.navigate('Atividades')}
+            />
         </View>
     )
 }
